d201e5bb5e3e24ea049099b12fcfcff291ec96bc
Arafat37 <arafatsarkar22@gmail.com>
2022-04-30 17:26:05 +0600

    send update quantity

--- src/Components/Inventory/Inventory.js
+++ src/Components/Inventory/Inventory.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { BsStarFill } from 'react-icons/bs'
+import { BsStarFill, BsPlusLg } from 'react-icons/bs'
 
 const Inventory = () => {
   const [product, setProduct] = useState([]);
+  const [update, setUpdate] = useState(false);
   const {_id} = useParams();
   useEffect(() => {
     const func = async() => {
@@ -15,14 +16,35 @@ const Inventory = () => {
     func()
   }, [])
 
-  console.log(product)
   const { img, name, description, price, quantity, supplier } = product;
+  const [prodQuantity, setQuantity] = useState(0);
+  const [addQuantity, setAddQuantity] = useState(0);
+
+  const increaseQuantity = () => {
+    const proceed = window.confirm('are sure?')
+    if(prodQuantity && proceed){
+      setQuantity( prodQuantity + Number(addQuantity));
+      const func = async() => {
+        const url = `http://localhost:5000/update?quantity=${prodQuantity + Number(addQuantity)}`
+        const { data } = await axios.put(url);
+      }
+      func()
+    }
+    else if(proceed){
+      setQuantity(quantity + Number(addQuantity));
+      const func = async() => {
+        const url = `http://localhost:5000/update?quantity=${quantity + Number(addQuantity)}`
+        const { data } = await axios.put(url);
+      }
+      func()
+    }
+  }
   
   return (
     <div className='min-h-screen bg-[#F7F4F2]'>
       <div className='lg:w-[65%] mx-auto grid lg:grid-cols-2 justify-items-center items-center gap-5'>
         <div className='bg-white w-[100%] h-[100%] flex justify-center p-8'>
-          <img src={img} alt="product" />
+          <img className='w-[80%] object-contain' src={img} alt="product" />
         </div>
         <div className='bg-white w-[100%] h-[100%] p-8'>
           <h1 className='font-bold text-lg'>{name}</h1>
@@ -31,8 +53,17 @@ const Inventory = () => {
           <p className='flex items-center font-bold'><BsStarFill className='mr-[.1rem]'/><BsStarFill className='mr-[.1rem]'/><BsStarFill className='mr-[.1rem]'/><BsStarFill className='mr-[.1rem]'/><BsStarFill className='mr-[.5rem]'/> 1 review</p>
           <p className='font-bold'>Supplier: {supplier}</p>
           <p>Description: {description}</p>
-          <div className='mt-5'>
-          <p className='font-bold'>Quantity: <span className='px-5 py-[.4rem] border'>{quantity}</span></p>
+          <div className='mt-5 flex items-center'>
+          <p className='font-bold'>Quantity: <span className='px-5 py-[.47rem] border ml-3'>{prodQuantity ? prodQuantity : quantity}</span></p>
+          {
+            update && <div className='flex'><p className='border px-3 py-[.57rem]'><BsPlusLg/></p>
+            <input onBlur={(e) => setAddQuantity(e.target.value)} placeholder='00' className='border w-[3.4rem] px-3 py-[.35rem] font-bold font-mono focus:outline-none' type="text" name="" id="" />
+            <button onClick={increaseQuantity} className='ml-5 px-5 font-bold font-mono text-[#3A2A2F] border rounded-lg hover:bg-[#F7F4F2]'>Confirm</button></div>
+          }
+          </div>
+          <div className='mt-[3rem]'>
+            <button onClick={() => setUpdate(true)} className='px-5 py-2 border w-[50%] font-bold font-mono text-[#3A2A2F] hover:bg-[#F7F4F2]'>Update</button>
+            <button className='px-5 py-2 border w-[50%] font-bold font-mono text-[#3A2A2F] hover:bg-[#F7F4F2]'>Delivered</button>
           </div>
         </div>
       </div>
