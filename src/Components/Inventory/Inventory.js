6683470ef7853975b9ad20ec035bd4a24df7049c
Arafat37 <arafatsarkar22@gmail.com>
2022-04-30 13:40:31 +0600

    added prod description

--- src/Components/Inventory/Inventory.js
+++ src/Components/Inventory/Inventory.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { BsStarFill } from 'react-icons/bs'
 
 const Inventory = () => {
   const [product, setProduct] = useState([]);
@@ -18,9 +19,22 @@ const Inventory = () => {
   const { img, name, description, price, quantity, supplier } = product;
   
   return (
-    <div>
-      <div>
-        <img src={img} alt="product" />
+    <div className='min-h-screen bg-[#F7F4F2]'>
+      <div className='lg:w-[65%] mx-auto grid lg:grid-cols-2 justify-items-center items-center gap-5'>
+        <div className='bg-white w-[100%] h-[100%] flex justify-center p-8'>
+          <img src={img} alt="product" />
+        </div>
+        <div className='bg-white w-[100%] h-[100%] p-8'>
+          <h1 className='font-bold text-lg'>{name}</h1>
+          <h4 className='font-bold text-lg'>Price: ${price}</h4>
+          <p className='font-bold'>Product ID: {_id.split(':')[1]}</p>
+          <p className='flex items-center font-bold'><BsStarFill className='mr-[.1rem]'/><BsStarFill className='mr-[.1rem]'/><BsStarFill className='mr-[.1rem]'/><BsStarFill className='mr-[.1rem]'/><BsStarFill className='mr-[.5rem]'/> 1 review</p>
+          <p className='font-bold'>Supplier: {supplier}</p>
+          <p>Description: {description}</p>
+          <div className='mt-5'>
+          <p className='font-bold'>Quantity: <span className='px-5 py-[.4rem] border'>{quantity}</span></p>
+          </div>
+        </div>
       </div>
     </div>
   );
